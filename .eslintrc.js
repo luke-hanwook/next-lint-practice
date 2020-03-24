2405356f9eb5d9078a845c75b5bb246cb46c82e1
Luke <gksdnr89@gmail.com>
2020-03-25 00:54:34 +0900

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     ],
     "rules": {
     },
-    "ignorePatterns": ["!.eslintrc.js"],
+    // "ignorePatterns": ["!.eslintrc.js"],
     "settings": {
         "react": {
             "version": "detect"
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 function Index() {
-  return <div>hello next.js</div>
+  return (
+    <>
+      <Link><a>about</a>
+      </Link>
+      <div>hello next.js</div>
+    </>
+  );
 }
 
-export default Index
+export default Index;
